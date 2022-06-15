86a8f3f5ad0c8fb55807c82761df47a47b1aff20
Ferr1s <ethfox56@gmail.com>
2022-06-14 20:09:16 -0400

    basic weather capability added

--- main.py
+++ main.py
@@ -1,31 +1,72 @@
+from turtle import width
 import pygame
 from datetime import datetime
+from bs4 import BeautifulSoup
+import requests
+import os
 
 def timegetter():
     now = datetime.now()
-    current_time = now.strftime("%I:%M %p")
+    current_time = now.strftime("%I:%M:%S %p")
     return current_time
 
-print(timegetter())
+bgcolor = (56, 167, 226)
+black = (0, 0, 0)
+white = (255, 255, 255)
 
-bgcolor = (14, 12, 81)
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+res = requests.get(f'https://www.google.com/search?q=Lansdale+weather&oq=Lansdale+weather&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8',headers=headers)
+soup = BeautifulSoup(res.text,'html.parser')   
+info = soup.select('#wob_dc')[0].getText().strip() 
+degrees = str(soup.select('#wob_tm')[0].getText().strip())
+
+weather = {
+    "cloudy" : "cloud.png",
+    "sunny" : "sun.png",
+    "clear" : "sun.png",
+    "mostly sunny" : "mostlysunny.png",
+    "partly cloudy" : "mostlysunny.png",
+    "scattered showers" : "showers.png"
+
+}
 
 pygame.init()
 
-font = pygame.font.Font("fonts\ShareTechMono.ttf", 150)
-win = pygame.display.set_mode((1280, 720))
+clockFont = pygame.font.Font(os.path.join("fonts", "ShareTechMono.ttf"), 150)
+weatherFont = pygame.font.Font(os.path.join("fonts", "ShareTechMono.ttf"), 180)
+width = 1280
+height = 720
+win = pygame.display.set_mode((width, height))
+
+screen1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
 
 pygame.display.set_caption("Dashboard")
 
 run = True
 while run:
-    win.fill(bgcolor)
+    now = datetime.now()
+    current_seconds = int(now.strftime("%S"))
 
-    testText = font.render(timegetter(), True, (255, 255, 255))
-    testTextRect = testText.get_rect()
-    testTextRect.center = (640.5, 360.5)
+    if current_seconds in screen1:
+#    if True:
+        win.fill(bgcolor)
+        degreesText = weatherFont.render(degrees + "°F", True, black)
+        degreesTextRect = degreesText.get_rect()
+        degreesTextRect.center = (width/2, height/12*10)
+        weatherImg = pygame.image.load(os.path.join("images", weather[info.lower()]))
+        weatherImgRect = weatherImg.get_rect()
+        weatherImgRect.center = (width/2, height/3)
 
-    win.blit(testText, testTextRect)
+        win.blit(weatherImg, weatherImgRect)
+        win.blit(degreesText, degreesTextRect)
+
+    else:
+        win.fill(bgcolor)
+        clockText = clockFont.render(timegetter(), True, black)
+        clockTextRect = clockText.get_rect()
+        clockTextRect.center = (width/2, height/2)
+
+        win.blit(clockText, clockTextRect)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
